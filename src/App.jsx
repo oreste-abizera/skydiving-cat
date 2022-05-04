43d22410182eaac03e25bbeb33f3e8602c913702
oreste-abizera <oresteabizera11@gmail.com>
2022-05-04 13:56:20 +0200

    feat: parallax content

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,59 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import moon from "./assets/moon.png";
+import land from "./assets/land.png";
+import cat from "./assets/cat.gif";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+
+import { useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const ref = useRef();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <Parallax pages={4} ref={ref}>
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          factor={2}
+          style={{
+            backgroundImage: `url(${moon})`,
+            backgroundSize: "cover",
+          }}
+        />
+
+        <ParallaxLayer
+          sticky={{ start: 0.9, end: 2.5 }}
+          style={{ textAlign: "center" }}
+        >
+          <img src={cat} alt="cat" />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={2}
+          speed={1}
+          factor={4}
+          style={{
+            backgroundImage: `url(${land})`,
+            backgroundSize: "cover",
+          }}
+        ></ParallaxLayer>
+
+        <ParallaxLayer
+          offset={0.2}
+          speed={0.05}
+          onClick={() => ref.current.scrollTo(3)}
+        >
+          <h2>Returning To Earth</h2>
+        </ParallaxLayer>
+        <ParallaxLayer
+          offset={3.2}
+          speed={2}
+          onClick={() => ref.current.scrollTo(0)}
+        >
+          <h2>Bye Moon!</h2>
+        </ParallaxLayer>
+      </Parallax>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
